7aa05aa92fe40924430819625289ecfd06d701a0
armen <armenbakir@hotmail.com>
2024-07-06 15:30:28 +0200

    added cors configuration

--- index.ts
+++ index.ts
@@ -1,9 +1,11 @@
 import express from "express";
 import categories from "./routes/categories";
 import foods from "./routes/foods";
+import cors from "cors";
 
 const app = express();
 
+app.use(cors({ origin: "http://localhost:5173" }));
 app.use(express.json());
 app.use("/api/categories", categories);
 app.use("/api/foods", foods);
